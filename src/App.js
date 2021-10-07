7d59e826496c304e738564de2087209e91e19be7
NaganikaTankasala <ntankasa@emich.edu>
2021-10-07 05:56:45 +0000

    mywebsite

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <body>
+  <div class="wrapper">
+    
+    <div class="left">
+      <img class="profile-picture" src="file:///C:/Users/aradh/OneDrive/Desktop/index.html/profile.JPG" width="200"/>
+      <h1 class="name">Naganika Tankasala</h1>
+      <h4 class="subtitle">Master of Compter Science </h4>
+    </div>
+    
+    <div class="right">
+      <p class="bio">
+        <h5 class="education">Graduate in computer Science 
+          Diploma in Dental Technician/Mechanics
+        </h5>
+        <h6 class="experience">2 Yearsexperience as Backend Developer at Softapt Software and Consultaning pvt.ltd 
+        </h6>
+        
+        Thanks for visiting my website.
+      </p>
+      <ul class="social-links">
+        <li><a href="https://naganika7410@gmail.com/naganika7410@gmail.com">Google</a></li>
+        <li><a href="https://ntankasa@emich.edu/@ntankasa">EasternMichiganUniversity</a></li>
+      </ul>
+    </div>
+    
+  </div>
+</body>
+
+
     </div>
   );
 }
